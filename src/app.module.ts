95065294332be24e0aa2f1b0880c3cb1d8fb473d
agwenchez <agweraenock76@gmail.com>
2024-01-05 01:18:03 +0300

    feat: CRUD operations on categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MerchantsModule } from './merchants/merchants.module';
+import { CategoriesModule } from './categories/categories.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [AuthModule, MerchantsModule],
+  imports: [AuthModule, MerchantsModule, CategoriesModule, TagsModule],
 })
 export class AppModule {}
