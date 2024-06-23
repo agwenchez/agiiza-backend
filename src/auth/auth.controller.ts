7944ee8db604c50f55cfd0acafc2c4992da79b48
agwenchez <agweraenock76@gmail.com>
2024-06-23 06:23:12 +0300

    feat: customers and orders services

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,26 +8,34 @@ import {
   Delete,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { CreateAuthDto, CustomerLoginDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Public } from './decorator';
 
-@Controller('api/v1/auth')
+@Controller('api/v1/auth/customers')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
+  @Public()
+  @Post('signup')
+  signup(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
   }
 
-  @Get()
+  @Public()
+  @Post('login')
+  login(@Body() createAuthDto: CustomerLoginDto) {
+    return this.authService.login(createAuthDto);
+  }
+
+  @Get('')
   findAll() {
     return this.authService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  @Get(':phoneNumber')
+  findOne(@Param('phoneNumber') phoneNumber: string) {
+    return this.authService.findOne(phoneNumber);
   }
 
   @Patch(':id')
--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,43 @@
-export class CreateAuthDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAuthDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email;
+
+  @IsNotEmpty()
+  @IsString()
+  phoneNumber;
+
+  @IsNotEmpty()
+  @IsString()
+  gender;
+
+  @IsNotEmpty()
+  @IsString()
+  dateOfBirth;
+
+  @IsNotEmpty()
+  @IsString()
+  firstName;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName;
+
+  @IsNotEmpty()
+  @IsString()
+  password;
+}
+
+
+export class CustomerLoginDto {
+    @IsNotEmpty()
+    @IsString()
+    phoneNumber: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+  }
+  
